d45eb4cf7256bccda5aa6370fcdb339db9ce9f35
Matt Keane <mattpkeane@gmail.com>
2021-05-13 15:14:47 -0500

    navbar now shows log out option if user is logged in

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  currentUser: any;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.currentUser
+      .subscribe(user => this.currentUser = user);
   }
 
 }
