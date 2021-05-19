bca3bf2cfa832e781e45ca988b51c628fa51bf90
Matt Keane <mattpkeane@gmail.com>
2021-05-19 14:35:43 -0500

    added routing to new artist component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ArtistListComponent } from './artist-list/artist-list.component';
+import { NewArtistComponent } from './new-artist/new-artist.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,12 @@ const routes: Routes = [
   {
     path: 'artists',
     component: ArtistListComponent,
+    children: [
+      {
+        path: 'new',
+        component: NewArtistComponent,
+      },
+    ],
   }
 ];
 
