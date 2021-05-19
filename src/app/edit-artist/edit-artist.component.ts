ee7dc4be508b5aae7d26f20bd1747dbe5cb60776
Matt Keane <mattpkeane@gmail.com>
2021-05-19 15:42:13 -0500

    edit artist modal now populate witht he artist to edit's data

--- src/app/edit-artist/edit-artist.component.ts
+++ src/app/edit-artist/edit-artist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtistService } from '../artist.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-artist',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-artist.component.css']
 })
 export class EditArtistComponent implements OnInit {
-
-  constructor() { }
+  artistToEdit: any = {
+    name: '',
+    born: '',
+    still_alive: '',
+  };
+  constructor(
+    private artistService: ArtistService,
+    private route: ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.artistService.allArtists.subscribe(allArtists => {
+      const foundArtist = allArtists.find(artist => artist.id === id);
+      if (foundArtist) {
+        this.artistToEdit = foundArtist;
+      }
+    });
   }
 
 }
